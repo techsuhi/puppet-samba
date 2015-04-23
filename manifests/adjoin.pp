aec0e1c01f7cd5bb0926d60a5734e2906d7a279c
Frank Grötzner <frank@unforgotten.de>
2015-04-23 12:38:29 +0200

    "testjoin" works better than "status"

--- manifests/adjoin.pp
+++ manifests/adjoin.pp
@@ -3,7 +3,7 @@ class samba::adjoin {
   exec { "net_ads_join":
     environment => "ADS_CREDS=${::samba::adjoin_user}%${::samba::adjoin_pass}",
     command     => "/usr/bin/net ads join createcomputer=\"${::samba::adjoin_ou}\" -U \$ADS_CREDS",
-    unless      => "/usr/bin/net ads status -U \$ADS_CREDS",
+    unless      => '/usr/bin/net ads testjoin',
     require     => Class['samba::config'],
   }
 
